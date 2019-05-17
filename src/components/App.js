b4fee45da21296a64ea9c80f6c2b537ba462e3ea
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2019-05-17 20:08:42 +0500

    Final Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import ResourceList from "./ResourceList";
+import UserList from "./UserList";
 
-class App extends Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+const App = () => {
+  const [resource, setResource] = useState("posts");
+  return (
+    <div>
+      <UserList />
+      <div className="">
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("todos")}>Todos</button>
+      </div>
+      <ResourceList resource={resource} />
+    </div>
+  );
+};
 
 export default App;
